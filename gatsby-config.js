da526114e56996c595d68c8b549868fe6563f899
Manoj Fernando <manojf@99x.lk>
2020-05-27 10:19:26 +0530

    Changed site's meta description

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
     title: `Winning Product`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    description: `Your tool kit to build a winning software product`,
+    author: `Winning Product Contributors`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
