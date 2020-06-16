60b7b96328143a9bcb102029022ff1c49df2a0ac
Charith Wickramasinghe <charithw@compello.com>
2020-06-17 01:29:39 +0530

    added siteconfig and redirecting to learn portal in mobile view

---
+++ site-config.js
@@ -0,0 +1,52 @@
+module.exports = {
+  Stages: [
+    {
+      name: "EXPLORE",
+      learnPortalLinks: {
+        main: "1-explore",
+      },
+    },
+    {
+      name: "FOCUS",
+      learnPortalLinks: {
+        main: "2-focus",
+      },
+    },
+    {
+      name: "IMMERSE",
+      learnPortalLinks: {
+        main: "3-immerse",
+      },
+    },
+    {
+      name: "PLAN",
+      learnPortalLinks: {
+        main: "4-plan",
+      },
+    },
+    {
+      name: "BUILD",
+      learnPortalLinks: {
+        main: "5-build",
+      },
+    },
+    {
+      name: "OPTIMIZE",
+      learnPortalLinks: {
+        main: "6-optimize",
+      },
+    },
+    {
+      name: "HARVEST",
+      learnPortalLinks: {
+        main: "7-harvest",
+      },
+    },
+    {
+      name: "RETIRE",
+      learnPortalLinks: {
+        main: "8-retire",
+      },
+    },
+  ],
+}
